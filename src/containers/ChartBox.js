1d77ac1677f5e98a673e0c019ab284c324b94f35
Jim Bevington <jim.bevington@hotmail.com>
2018-04-24 20:49:57 +0100

    added fetch for top20

--- src/containers/ChartBox.js
+++ src/containers/ChartBox.js
@@ -8,6 +8,12 @@ class ChartBox extends React.Component {
     }
   }
 
+  componentDidMount(){
+    fetch('https://itunes.apple.com/gb/rss/topsongs/limit=20/json')
+      .then(response => response.json())
+      .then(json => this.setState({top20: json.feed.entry}));
+  }
+
   render(){
     return <h1>Hello World</h1>
   }
