eb5329976d0120dba74740ccefd3c783b825f931
Jim Bevington <jim.bevington@hotmail.com>
2018-04-24 20:38:34 +0100

    deleted react logo stuff, created file strucuture, added Hello World test to ChartBox

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ChartBox from './containers/ChartBox';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <ChartBox />
+    );
+  }
+}
+
+export default App;
