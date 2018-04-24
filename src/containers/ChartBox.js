7699bdb67c3e5de8ef653d7e97cf18d1e841fc7e
Jim Bevington <jim.bevington@hotmail.com>
2018-04-24 20:40:35 +0100

    added constructor to ChartBox

--- src/containers/ChartBox.js
+++ src/containers/ChartBox.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class ChartBox extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      top20: []
+    }
+  }
 
   render(){
     return <h1>Hello World</h1>
