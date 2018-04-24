c932bc0c802f7dd11e72ffc23ff0949f465d970d
Jim Bevington <jim.bevington@hotmail.com>
2018-04-24 21:49:33 +0100

    added song title and artist as properties to ChartEntry and displayed them

--- src/components/ChartEntry.js
+++ src/components/ChartEntry.js
@@ -3,7 +3,9 @@ import React from 'react';
 const ChartEntry = (props) => {
   return (
     <li>
-      <h1>{props.position + 1}</h1>
+      <h2 className="chart-position">{props.position}</h2>
+      <h3 className="song-title">{props.title}</h3>
+      <h3 className="artist">{props.artist}</h3>
     </li>
   )
 }
--- src/components/ChartList.js
+++ src/components/ChartList.js
@@ -4,8 +4,18 @@ import ChartEntry from './ChartEntry';
 const ChartList = (props) => {
 
   const chartEntries = props.top20.map((entry, index) => {
-    return <ChartEntry key={index} position={index} />
+    return (
+      <ChartEntry
+        key={index}
+        position={index + 1}
+        title={entry['im:name']['label']}
+        artist={entry['im:artist']['label']}
+      />
+    )
   })
+
+  // const entry = props.top20[0];
+  // console.log(entry);
 
   return (
     <ul>
